798cd4078a6e23d61166fcb0b891fafd088ea23d
JukisYuri <23130092@st.hcmuaf.edu.vn>
2024-11-17 14:42:37 +0700

    Finished

--- src/DataStructure/Lab8/set_student/MyPredicates.java
+++ src/DataStructure/Lab8/set_student/MyPredicates.java
@@ -1,6 +1,7 @@
 package DataStructure.Lab8.set_student;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.function.Predicate;
@@ -15,6 +16,7 @@ public class MyPredicates {
 			T next = iter.next();
 			if(p.test(next))
 				iter.remove();
+			// xoá các phần tử thoả mãn điều kiện p
 		}
 	}
 
@@ -22,22 +24,42 @@ public class MyPredicates {
 	// pr.test(obj) is false. (That is, retain the
 	// objects for which the predicate is true.)
 	public static <T> void retain(Collection<T> coll, Predicate<T> p) {
-		// TODO
+		Iterator<T> result = coll.iterator();
+		while(result.hasNext()){
+			T next = result.next();
+			if(!p.test(next)){
+				result.remove();
+				// giữ các phần tử thoả mãn điều kiện p
+			}
+		}
 	}
 
 	// Return a Set that contains all unique objects, obj,
 	// from the collection, coll, such that p.test(obj)
 	// is true.
 	public static <T> Set<T> collect(Collection<T> coll, Predicate<T> p) {
-		// TODO
-		return null;
+		Set<T> result = new HashSet<>();
+		for(T t_coll : coll){
+			if(p.test(t_coll)){
+				result.add(t_coll);
+			}
+		}
+		return result;
 	}
 
 	// Return the index of the first item in list
 	// for which the predicate is true, if any.
 	// If there is no such item, return -1.
 	public static <T> int find(Collection<T> coll, Predicate<T> p) {
-		// TODO
+		Iterator<T> result = coll.iterator();
+		int index = 0;
+		while(result.hasNext()){
+			T next = result.next();
+			if(p.test(next)){
+				return index;
+			}
+			index++;
+		}
 		return -1;
 	}
 }
--- src/DataStructure/Lab8/set_student/TestPredicates.java
+++ src/DataStructure/Lab8/set_student/TestPredicates.java
@@ -1,9 +1,6 @@
 package DataStructure.Lab8.set_student;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 
 public class TestPredicates {
 	public static void main(String[] args) {
@@ -16,5 +13,26 @@ public class TestPredicates {
 		System.out.println("Before: "+list);
 		MyPredicates.remove(list, new Even());
 		System.out.println("After: "+list);
+		list.clear();
+
+		Collections.addAll(list, 1,2,3,4,5);
+		MyPredicates.retain(list, new Even());
+		System.out.println("Retain: " + list);
+		list.clear();
+
+		Set<Integer> listSet = new HashSet<>();
+		Collections.addAll(listSet, 1, 2, 3, 4, 5);
+		Set<Integer> collected = MyPredicates.collect(listSet, new Even());  // Lưu kết quả vào biến mới
+		System.out.println("Collect: " + collected);
+		list.clear();
+
+		Collections.addAll(list, 1, 2, 3, 4, 5);
+		int index = MyPredicates.find(list, new Even());
+		if (index != -1) {
+			System.out.println("Find: The first even number is at index " + index + " with value " + list.toArray()[index]);
+		} else {
+			System.out.println("Find: No even number found.");
+		}
+		list.clear();
 	}
 }
